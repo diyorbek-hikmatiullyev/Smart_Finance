991bc580a8fd6628d668518b79cbe012c5bb1835
diyorbek-hikmatiullyev <Diyorbek192004@icloud.com>
2026-04-07 23:27:00 +0500

    Deleted goal codes

--- SmartFinance/Views/Goals/FinancialGoal.swift
+++
@@ -1,8 +0,0 @@
-//
-//  FinancialGoal.swift
-//  SmartFinance
-//
-//  Created by Diyorbek Xikmatullayev on 07/04/26.
-//
-
-import Foundation
--- SmartFinance/Views/Goals/Goalslistview.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Goalslistview.swift
-//  SmartFinance
-//
-//  Created by Diyorbek Xikmatullayev on 07/04/26.
-//
-
-import Foundation
