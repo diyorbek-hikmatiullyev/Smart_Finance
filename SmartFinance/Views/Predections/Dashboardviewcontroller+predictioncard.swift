24ee546eb4a6e9a8b132c84803dc2e5cf0f71877
diyorbek-hikmatiullyev <Diyorbek192004@icloud.com>
2026-04-09 13:57:00 +0500

    added changes

--- SmartFinance/Views/Predections/Dashboardviewcontroller+predictioncard.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Dashboardviewcontroller+predictioncard.swift
-//  SmartFinance
-//
-//  Created by Diyorbek Xikmatullayev on 08/04/26.
-//
-
-import Foundation
--- SmartFinance/Views/Predections/SpendingPredictor.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SpendingPredictor.swift
-//  SmartFinance
-//
-//  Created by Diyorbek Xikmatullayev on 08/04/26.
-//
-
-import Foundation
